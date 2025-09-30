87ee35544c832a9c40063b23be21394dc1ebdba9
Lucas Huang <lucas.huang@menloschool.org>
2025-09-29 21:24:55 -0700

    Changed to binary sort

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Spell Check
  * A puzzle written by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Lucas Huang
  * */
 
 public class SpellCheck {
@@ -18,7 +21,13 @@ public class SpellCheck {
      * @return String[] of all mispelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
-
-        return null;
+        ArrayList<String> misspelled = new ArrayList<String>();
+        for (int i = 0; i < text.length; i++) {
+            int index = Arrays.binarySearch(dictionary, text[i]);
+            if (index < 0 && !misspelled.contains(text[i])) {
+                misspelled.add(text[i]);
+            }
+        }
+        return misspelled.toArray(new String[0]);
     }
 }

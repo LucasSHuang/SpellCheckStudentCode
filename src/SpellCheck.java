2a949cadf0c77eb052d2552eebb08f5b66631af6
Lucas Huang <lucas.huang@menloschool.org>
2025-09-30 20:30:53 -0700

    Added some constants

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -11,8 +11,19 @@ import java.util.Arrays;
  * */
 
 public class SpellCheck {
-
-
+    private static final int A_START = 1;
+    private static final int B_START = 9130;
+    private static final int C_START = 16743;
+    private static final int D_START = 29785;
+    private static final int E_START = 38323;
+    private static final int F_START = 44385;
+    private static final int G_START = 49850;
+    private static final int H_START = 54355;
+    private static final int I_START = 59879;
+    private static final int J_START = 65618;
+    private static final int K_START = 66847;
+    private static final int L_START = 68384;
+    private static final int M_START = 72940;
     /**
      * checkWords finds all words in text that are not present in dictionary
      *
